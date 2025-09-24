7fb922d4492139fa884d045fdd7be020359896a8
Loïc Faugeron <faugeron.loic@gmail.com>
2025-09-24 09:08:33 +0100

    Fixed prepare.sh

--- _sculpin/prepare.sh
+++ _sculpin/prepare.sh
@@ -18,10 +18,10 @@ echo "  // Preparing last article for publication"
 
 _BLOG_LATEST_ARTICLE=$(ls source/_posts/*.md | sort | tail -n 1)
 _BLOG_LATEST_ARTICLE_TITLE=$(grep '^title:' "${_BLOG_LATEST_ARTICLE}" | sed 's/^title: *"*//' | sed 's/"*$//')
+_BLOG_BRANCH=$(git branch --show-current)
 git add -A
 git commit -m "Prepared \"${_BLOG_LATEST_ARTICLE_TITLE}\""
-_BLOG_BRANCH=$(git branch --show-current) && \
-    git checkout main && \
+git checkout main && \
     git merge --no-ff $_GIT_BRANCH && \
     git branch -D $_GIT_BRANCH && \
     git push
